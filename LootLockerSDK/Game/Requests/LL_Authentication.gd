75bc91e2d3f878d74f15581bab93d32fd3db81ed
Erik Bylund <erik@lootlocker.io>
2024-11-28 08:51:41 +0100

    feature: Make steam auth use new steam auth endpoint

--- LootLockerSDK/Game/Requests/LL_Authentication.gd
+++ LootLockerSDK/Game/Requests/LL_Authentication.gd
@@ -19,17 +19,17 @@ class LL_GuestSessionRequest extends LootLockerInternal_BaseRequest:
 	var game_key : String
 	var game_version : String
 	
-	func _init(_player_identifier : String, _game_key : String, _game_version : String):
+	func _init(_player_identifier : String):
 		player_identifier = _player_identifier
-		game_key = _game_key
-		game_version = _game_version
+		game_key = LootLockerInternal_Settings.GetApiKey()
+		game_version = LootLockerInternal_Settings.GetGameVersion()
 
 class GuestSession extends LootLockerInternal_RequestDefinition:
 	func _init(playerIdentifier : String = "") -> void:
 		responseType = LL_BaseSessionResponse
 		if(playerIdentifier == ""):
 			playerIdentifier = LootLockerInternal_LootLockerCache.current().get_data("player_identifier", "")
-		request = LL_GuestSessionRequest.new(playerIdentifier, LootLockerInternal_Settings.GetApiKey(), LootLockerInternal_Settings.GetGameVersion())
+		request = LL_GuestSessionRequest.new(playerIdentifier)
 		super._init("/game/v2/session/guest", LootLockerInternal_HTTPClient.http_methods.POST, __SESSION_RESPONSE_FIELDS_TO_CACHE)
 	
 	func send() -> LL_BaseSessionResponse:
@@ -37,22 +37,30 @@ class GuestSession extends LootLockerInternal_RequestDefinition:
 		return response
 	
 class LL_SteamSessionRequest extends LootLockerInternal_BaseRequest:
-	var player_identifier : String
-	var platform : String
-	var game_key : String
+	var game_api_key : String
 	var game_version : String
+	var steam_ticket : String
 	
-	func _init(_player_identifier : String, _game_key : String, _game_version : String, _platform : String):
-		player_identifier = _player_identifier
-		game_key = _game_key
-		game_version = _game_version
-		platform = _platform
+	func _init(_steam_ticket : String):
+		steam_ticket = _steam_ticket
+		game_api_key = LootLockerInternal_Settings.GetApiKey()
+		game_version = LootLockerInternal_Settings.GetGameVersion()
+	
+class LL_SteamSessionRequestWithAppId extends LL_SteamSessionRequest:
+	var steam_app_id : String
+	
+	func _init(_steam_ticket : String, _steam_app_id : String = ""):
+		steam_app_id = _steam_app_id
+		super._init(_steam_ticket)
 
 class SteamSession extends LootLockerInternal_RequestDefinition:
-	func _init(playerIdentifier : String) -> void:
+	func _init(steamTicket : String, steamAppId : String = "") -> void:
 		responseType = LL_BaseSessionResponse
-		request = LL_SteamSessionRequest.new(playerIdentifier, LootLockerInternal_Settings.GetApiKey(), LootLockerInternal_Settings.GetGameVersion(), "steam")
-		super._init("/game/v2/session", LootLockerInternal_HTTPClient.http_methods.POST, __SESSION_RESPONSE_FIELDS_TO_CACHE)
+		if !steamAppId.is_empty():
+			request = LL_SteamSessionRequestWithAppId.new(steamTicket, steamAppId)
+		else:
+			request = LL_SteamSessionRequest.new(steamTicket)
+		super._init("/game/session/steam", LootLockerInternal_HTTPClient.http_methods.POST, __SESSION_RESPONSE_FIELDS_TO_CACHE)
 		
 	func send() -> LL_BaseSessionResponse:
 		await _send()
