e60787013a935d880e0e68d7b6d64c10e664d102
Erik Bylund <erik@lootlocker.io>
2025-01-27 11:05:41 +0100

    feature: Make LootLockerSDK an actual plugin

--- LootLockerSDK/Game/Internals/LootLockerInternal_Settings.gd
+++ LootLockerSDK/Game/Internals/LootLockerInternal_Settings.gd
@@ -15,7 +15,6 @@
 ##  game_version="1.2.1.4"
 ## [/codeblock]
 ##[br][color=light green][b]Copyright (c) 2024 LootLocker.[/b][/color]
-@tool
 class_name LootLockerInternal_Settings
 extends Resource
 
@@ -55,6 +54,9 @@ static func GetInstance() -> LootLockerInternal_Settings:
 	return _instance
 
 func _init() -> void:
+	loadSettings()
+
+func loadSettings() -> void:
 	var settings = ConfigFile.new()
 	var err = settings.load(SETTINGS_PATH)
 	if ( err != OK ):
